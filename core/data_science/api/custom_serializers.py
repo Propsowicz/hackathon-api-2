a0fa233629c8de60c38b9801a0387b073c3a177d
Tomasz Tomasik <tomasz_tomasik@yahoo.com>
2022-12-03 07:14:25 +0100

    deploy_7:14

--- core/data_science/api/custom_serializers.py
+++ core/data_science/api/custom_serializers.py
@@ -213,12 +213,13 @@ def pearson_corr(data_dict):
     matrix_list = []
     for y_scale in Pcorr:
         matrix_list.append({y_scale: []})
-        for x_scale, val in zip(Pcorr, np.array(Pcorr)[i]):
-            if np.isnan(val):
+        j = 0
+        for x_scale, val in zip(Pcorr, np.array(Pcorr)[i]):                           
+            if np.isnan(val) and j != 0:
                 matrix_list[i][y_scale].append({'x': x_scale, 'y':0})
-            else:
+            elif j != 0:
                 matrix_list[i][y_scale].append({'x': x_scale, 'y':val})
+            j += 1            
         i += 1
-
     return matrix_list[0]
 
